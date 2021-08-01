39ac5b37447d3432b69886a9fb402e3525f4663b
oktayafandi <ogtayaa@code.edu.az>
2021-08-02 01:55:34 +0400

    Place Creat yazildi

--- neyeyim/neyeyim/Areas/Manage/Controllers/PlaceController.cs
+++ neyeyim/neyeyim/Areas/Manage/Controllers/PlaceController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using neyeyim.DAL;
 using neyeyim.Models;
@@ -13,9 +15,11 @@ namespace neyeyim.Areas.Manage.Controllers
     public class PlaceController : Controller
     {
         private readonly AppDbContext _context;
-        public PlaceController(AppDbContext context)
+        private readonly IWebHostEnvironment _env;
+        public PlaceController(AppDbContext context, IWebHostEnvironment env)
         {
             _context = context;
+            _env = env;
         }
 
         public IActionResult Index(int page = 1)
@@ -23,8 +27,45 @@ namespace neyeyim.Areas.Manage.Controllers
             ViewBag.SelectedPage = page;
             ViewBag.TotalPageCount = Math.Ceiling(_context.Categories.Count() / 3d);
 
-            List<Place> places = _context.Places.Include(x => x.Campaigns).Include(x => x.Jobads).Include(x => x.PlaceTags).Include(x => x.PlaceMenus).Skip((page - 1) * 3).Take(3).ToList();
+            List<Place> places = _context.Places.Include(x => x.Category).Include(x => x.Campaigns).Include(x => x.Jobads).Include(x => x.PlaceTags).Include(x => x.PlaceMenus).Skip((page - 1) * 3).Take(3).ToList();
             return View(places);
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.Categories = _context.Categories.Where(x => x.IsDeleted == false).Select(a => new SelectListItem
+            {
+                Value = a.Id.ToString(),
+                Text = a.Name,
+                Selected = true
+            }).ToList();
+
+            return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public IActionResult Create(Place place)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _context.Places.Add(place);
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Place place = _context.Places.FirstOrDefault(x => x.Id == id);
+
+            if (place == null) return RedirectToAction("index");
+            ViewBag.Categories = _context.Categories.ToList();
+
+            return View(place);
         }
     }
 }

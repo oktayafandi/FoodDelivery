85f24f98bd06f5ba6cd76ae3225b508bd8600944
oktayafandi <ogtayaa@code.edu.az>
2021-08-21 02:03:55 +0400

    Order hissesi yazildi

---
+++ neyeyim/neyeyim/Areas/Manage/Controllers/OrderController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using neyeyim.DAL;
+using neyeyim.Enums;
+using neyeyim.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace neyeyim.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    [Authorize(Roles = "Admin, SuperAdmin")]
+    public class OrderController : Controller
+    {
+        private readonly AppDbContext _context;
+        public OrderController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(int page = 1)
+        {
+            ViewBag.SelectedPage = page;
+            ViewBag.TotalPage = Math.Ceiling(_context.OrderItems.Count() / 3d);
+            var model = _context.Orders.Include(x => x.OrderItems).OrderByDescending(x => x.CreatedAt).Skip((page - 1) * 3).Take(3).ToList();
+            return View(model);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Order order = _context.Orders.Include(x => x.OrderItems).FirstOrDefault(x => x.Id == id);
+            if (order == null)
+            {
+                return RedirectToAction("index");
+            }
+            return View(order);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ChangeStatus(int id, OrderStatus status)
+        {
+            Order order = _context.Orders.FirstOrDefault(x => x.Id == id);
+            if (order == null)
+            {
+                return RedirectToAction("index");
+            }
+            order.Status = status;
+            _context.SaveChanges();
+            return RedirectToAction("index");
+        }
+    }
+}
